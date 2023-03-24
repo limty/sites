02958bc1ba1df916ef892e650ee954f0e7a7a15c
Yew Lim <8343404+limty@users.noreply.github.com>
2023-03-25 01:43:07 +0800

    Update demo survey form

--- demo-survey-form/script.js
+++ demo-survey-form/script.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("survey-form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  alert("Thanks for your feedback!");
+  alert("Thanks for trying out the form!");
+  location.reload();
 });
